cb623183db6a32c8c66c8c118068b4a482b2753e
zidehen <zoeidehen@gmail.com>
2021-11-16 17:30:38 +0000

    Step 9 - defined working? method

---
+++ spec/bike_spec.rb
@@ -0,0 +1,4 @@
+require "bike"
+describe Bike do
+  it { expect(subject).to respond_to(:working?) }
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,4 @@
-require 'boris-bikes'
+require 'docking_station'
 
 describe DockingStation do
   it { expect(subject).to respond_to(:release_bike) }
