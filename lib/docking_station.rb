d70e495e02e077ed9612d2e9231c69f9daf6e901
Jessica Gordon <ketchesj@gmail.com>
2021-11-17 15:35:40 +0000

    Step 10: working? method and release_bike method now passing rspec

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
+require_relative 'bike'
+
 class DockingStation
   def release_bike
+    bike = Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { expect(subject).to respond_to(:release_bike) }
+  it { expect(subject.release_bike).to be_an_instance_of(Bike) }
+  it { expect(subject.release_bike.working?).to be(true) }
 end
