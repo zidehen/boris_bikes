dae2a4e198f525d4d1f63274e1fa5b5187bc468f
zidehen <zoeidehen@gmail.com>
2021-11-17 17:11:10 +0000

    Step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,23 @@
 require_relative 'bike'
 
 class DockingStation
+  
+  attr_reader :bike_available
+
+  def initialize
+    @bike_available = []
+  end
+
   def release_bike
-    bike = Bike.new
+    if bike_available.empty?
+      raise "no bikes available"
+    else
+      bike = bike_available.pop
+    end 
+  end
+
+  def dock(bike)
+    @bike_available.push(bike)
+    
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,14 @@ require 'bike'
 describe DockingStation do
   it { expect(subject).to respond_to(:release_bike) }
   it { expect(subject.release_bike).to be_an_instance_of(Bike) }
-  it { expect(subject.release_bike.working?).to be(true) }
+  it { expect(subject). to respond_to :dock }
+
+  it 'should be able to dock a bike and return bikes available' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike_available).to include(bike)
+  end
+  
+  it { expect { subject.release_bike }.to raise_error("no bikes available") }
+
 end
