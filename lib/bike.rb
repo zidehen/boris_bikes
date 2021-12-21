f1ca37401110c943a1f40164a60300b644e25ff4
zidehen <zoeidehen@gmail.com>
2021-12-21 10:13:00 +0000

    added methods to report broken bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -3,4 +3,12 @@ class Bike
   def working?
     true
   end
+
+  def report_broken
+    @broken = true
+  end
+
+  def broken?
+    @broken
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,10 @@ require "bike"
 describe Bike do
   it { is_expected.to respond_to :working? }
   it { expect(subject.working?).to be (true) }
+
+  it 'can report a bike as broken when returned' do
+    bike = Bike.new
+    bike.report_broken
+    expect(bike).to be_broken
+  end
 end
